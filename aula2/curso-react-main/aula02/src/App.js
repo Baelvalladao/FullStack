cb591a217d65eecc83427c7fb319772e0cfd0556
Bael <valladaobael@outlook.com>
2024-09-24 21:57:04 -0300

    AULA SEMANA 8

--- aula2/curso-react-main/aula02/src/App.js
+++ aula2/curso-react-main/aula02/src/App.js
@@ -1,40 +1,50 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
+import axios from "axios";
 
-const {nome, email} = {nome: 'Gabriel', email: 'djfn@email.com'}
-
-const Dezenas = (cliques) => {
-  useEffect(() => {
-    console.log(`dezenas: ${cliques}`);
-
-    return () => console.log(`dezenas - descarga: ${cliques}`);
-  }, []);
-  return <h2>{parseInt(cliques / 10)} dezenas + ${cliques % 10}cliques.</h2>
+const listar = () => {
+  axios.get(`http://localhost:3005/alunos`).then(
+    resultado => {
+      console.log(resultado.data);
+    }
+  );
 }
-const Unidades = (cliques) => {
-  useEffect(() => {
-    console.log(`unidades: ${cliques}`);
-    return () => console.log(`unidades - descarga: ${cliques}`);
-  }, []);
-  return <h2>{cliques}.cliques.</h2>
+const consultarUnico = id => {
+  axios.get(`http://localhost:3005/alunos/${id}`).then(
+    resultado => {
+      console.log(resultado.data);
+    }
+  );
+}
+const excluir = id => {
+  axios.delete(`http://localhost:3005/alunos/${id}`).then(
+    resultado => {
+      console.log(resultado.status);
+    }
+  );
+}
+const inserir = () => {
+  const aluno = {id: "5", nome:"Carlos", matricula:"666"}
+  axios.post(`http://localhost:3005/alunos`, aluno).then(
+    resultado => {
+      console.log(resultado.status);
+    }
+  )
+}
+const alterar = id =>{
+  const aluno = {id: "5", nome:"Carlos", matricula:"666"}
+  axios.put(`http://localhost:3005/alunos/${"5"}`, aluno).then(
+    resultado => console.log(resultado.status)
+  )
 }
 
-function App = () => {
-  const [cliques, setCliques] = useState(0);
-
-
-  let painel;
-
-  if(cliques < 10){
-    painel = <Unidades cliques={cliques} />;
-  }else{
-    painel = <Dezenas cliques={cliques} />
-  }
-
+const App = () => {
   return (
     <div>
-      {painel}
-      <button onClick={() => setCliques(cliques + 1)}>Clique aqui</button>
-
+      <button onClick={listar}>Listar</button>
+      <button onClick={() => consultarUnico("1")} >Consultar único</button>
+      <button onClick={inserir} >Inserir</button>
+      <button onClick={()=> alterar(5)} >Alterar</button>
+      <button onClick={() => excluir(3)} >Excluir</button>
     </div>
   );
 }
